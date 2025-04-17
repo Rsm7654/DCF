06acef482b3a4a5fc3766a1a62ceb927782e19c1
Rsm7654 <150913716+Rsm7654@users.noreply.github.com>
2025-04-17 21:44:39 +0530

    Update price_chart.py

--- price_chart.py
+++ price_chart.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-def show_chart(ticker, symbol):
-    st.subheader(f"📈 Stock Price Chart - {symbol}")
+def show_chart(ticker):
+    st.subheader(f"📈 Stock Price Chart - {ticker}")
     hist = ticker.history(period="5y")
     st.line_chart(hist["Close"])
