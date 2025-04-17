671e957475d202941c1b064e0a22d8748b4aef4d
Rsm7654 <150913716+Rsm7654@users.noreply.github.com>
2025-04-17 23:11:48 +0530

    Update price_chart.py

--- price_chart.py
+++ price_chart.py
@@ -1,18 +1,6 @@
 import streamlit as st
 
-
-def show_chart(stock_name):
-    st.subheader(f"📈 Stock Price Chart - {stock_name}")
-
-    # Get 1 month data
-    ticker = yf.Ticker(stock_name)
-    hist = ticker.history(period="1mo")
-
-    # Calculate price change
-    price_change = hist["Close"][-1] - hist["Close"][0]
-    pct_change = (price_change / hist["Close"][0]) * 100
-
-    st.metric(label="Price", value=f"₹{hist['Close'][-1]:.2f}", delta=f"{price_change:.2f} ({pct_change:.2f}%)")
-
-    # Line chart
+def show_chart(ticker):
+    st.subheader(f"📈 Stock Price Chart - {ticker}")
+    hist = ticker.history(period="5y")
     st.line_chart(hist["Close"])
