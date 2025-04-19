5d6c488783d182ca4741c7bab42637b98280a7a4
Rsm7654 <150913716+Rsm7654@users.noreply.github.com>
2025-04-20 00:28:55 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -47,7 +47,7 @@ if ticker_symbol:
     tab1, tab2, tab3, tab4 = st.tabs(["Peer-to-peer Comparison", "💸 DCF Valuation", "📈 Price Chart", "📄 Financials"])
 
     with tab1:
-        peer_comparison(ticker)
+        def peer_comparison(ticker_symbol, df_stocks):
 
     with tab2:
         run_dcf(ticker)
